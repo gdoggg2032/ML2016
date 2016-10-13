21b4b65eb1a17c95e7655c4ba9ff89ccdc0ee230
gdoggg <gdoggg2032@gmail.com>
2016-10-13 20:31:21 +0800

    python2.7

--- hw1/kaggle_best.sh
+++ hw1/kaggle_best.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-python preprocess.py train.csv train.np 10 1
-python preprocess.py test_X.csv test.np 10 0
+python2.7 preprocess.py train.csv train.np 10 1
+python2.7 preprocess.py test_X.csv test.np 10 0
 
-python regression_kaggle_best.py train.np test.np ./kaggle_best.csv 1
+python2.7 regression_kaggle_best.py train.np test.np ./kaggle_best.csv 1
--- hw1/linear_regression.sh
+++ hw1/linear_regression.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
-python preprocess.py train.csv train.np 10 1
-python preprocess.py test_X.csv test.np 10 0
+python2.7 preprocess.py train.csv train.np 10 1
+python2.7 preprocess.py test_X.csv test.np 10 0
 
-python regression.py train.np test.np linear_regression.csv 1
+python2.7 regression.py train.np test.np linear_regression.csv 1
