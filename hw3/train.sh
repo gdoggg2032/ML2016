04d61ac008fb377bb6f752a0c9c1f0288ba6e3f6
gdoggg <gdoggg2032@gmail.com>
2016-11-15 03:21:09 +0800

    train.sh mode

--- hw3/train.sh
+++ hw3/train.sh
@@ -1,1 +1,1 @@
-python2.7 ./cnn_auto.py --label $1/all_label.p --unlabel $1/all_unlabel.p --test $1/test.p --model $2 --auto_model $2.auto
+python2.7 ./cnn_auto.py --mode 0 --label $1/all_label.p --unlabel $1/all_unlabel.p --test $1/test.p --model $2 --auto_model $2.auto
